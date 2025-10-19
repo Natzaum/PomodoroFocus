2f83d502390183bab5284daf2461d13cfa7b5510
Natzaum <natacezerbordignon@gmail.com>
2025-10-19 14:37:12 -0300

    feat: Initializing projetct

---
+++ PomodoroFocus/GlobalXmlns.cs
@@ -0,0 +1,5 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "PomodoroFocus")]
+[assembly: XmlnsDefinition(
+    "http://schemas.microsoft.com/dotnet/maui/global",
+    "PomodoroFocus.Pages"
+)]
